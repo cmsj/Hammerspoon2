6d99208efadf72262f75f91d6ceefb7f37320d07
Chris Jones <cmsj@tenshu.net>
2025-09-23 20:59:02 +0100

    Initial Commit

---
+++ Hammerspoon 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hammerspoon 2
+//
+//  Created by Chris Jones on 23/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hammerspoon 2/Hammerspoon_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hammerspoon_2App.swift
+//  Hammerspoon 2
+//
+//  Created by Chris Jones on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hammerspoon_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
