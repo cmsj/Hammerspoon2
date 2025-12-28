9b8228f386c4b77a8c76c067fb5d963fed133b0e
Daniel M German <dmg@turingmachine.org>
2025-12-28 07:16:39 -0800

    Add hs.ipc module and fix CFMessagePort memory crash
    * Add hs.ipc module for IPC communication
    * Fix critical crash: passUnretained → passRetained in HSMessagePort
    * Prevents EXC_BAD_ACCESS during rapid hs2 command execution
    * Add Swift 6 compatibility annotations (@preconcurrency @unsafe)
    
    Tested: 20+ rapid hs2 commands execute successfully without crashes

--- Hammerspoon 2/Engine/ModuleRoot.swift
+++ Hammerspoon 2/Engine/ModuleRoot.swift
@@ -21,6 +21,7 @@ import JavaScriptCoreExtras
     @objc var console: HSConsoleModule { get }
     @objc var hashing: HSHashModule { get }
     @objc var hotkey: HSHotkeyModule { get }
+    @objc var ipc: HSIPCModule { get }
     @objc var permissions: HSPermissionsModule { get }
     @objc var task: HSTaskModule { get }
     @objc var timer: HSTimerModule { get }
@@ -75,6 +76,7 @@ import JavaScriptCoreExtras
     @objc var console: HSConsoleModule { get { getOrCreate(name: "console", type: HSConsoleModule.self)}}
     @objc var hashing: HSHashModule { get { getOrCreate(name: "hashing", type: HSHashModule.self)}}
     @objc var hotkey: HSHotkeyModule { get { getOrCreate(name: "hotkey", type: HSHotkeyModule.self)}}
+    @objc var ipc: HSIPCModule { get { getOrCreate(name: "ipc", type: HSIPCModule.self)}}
     @objc var permissions: HSPermissionsModule { get { getOrCreate(name: "permissions", type: HSPermissionsModule.self)}}
     @objc var task: HSTaskModule { get { getOrCreate(name: "task", type: HSTaskModule.self)}}
     @objc var timer: HSTimerModule { get { getOrCreate(name: "timer", type: HSTimerModule.self)}}
