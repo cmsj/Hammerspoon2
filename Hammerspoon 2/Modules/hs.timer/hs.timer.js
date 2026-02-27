aa11ff0832c984ce1e0dc4411c5f1230378f1590
Chris Jones <cmsj@tenshu.net>
2026-02-27 09:43:26 +0000

    Remove some unnecessary things from hs.timer

--- Hammerspoon 2/Modules/hs.timer/hs.timer.js
+++ Hammerspoon 2/Modules/hs.timer/hs.timer.js
@@ -39,52 +39,6 @@ hs.timer.weeks = function(n) {
     return n * 604800;
 };
 
-// Parse time strings
-// Supports formats like "HH:MM:SS", "HH:MM", "5m", "2h", "1d", etc.
-// FIXME: Decide if we want this or not, and if its name should change. If we want it, document it.
-/// SKIP_DOCS
-hs.timer.seconds = function(timeString) {
-    if (typeof timeString !== 'string') {
-        throw new Error("hs.timer.seconds(): argument must be a string");
-    }
-
-    // Try parsing as HH:MM:SS or HH:MM format (time since midnight)
-    const timeMatch = timeString.match(/^(\d{1,2}):(\d{2})(?::(\d{2}))?$/);
-    if (timeMatch) {
-        const hours = parseInt(timeMatch[1]);
-        const minutes = parseInt(timeMatch[2]);
-        const seconds = timeMatch[3] ? parseInt(timeMatch[3]) : 0;
-
-        if (hours >= 24 || minutes >= 60 || seconds >= 60) {
-            throw new Error("hs.timer.seconds(): invalid time string (hours must be < 24, minutes/seconds < 60)");
-        }
-
-        return hours * 3600 + minutes * 60 + seconds;
-    }
-
-    // Try parsing as duration format: "5m", "2h", "1d", "30s", "500ms"
-    const durationMatch = timeString.match(/^(\d+(?:\.\d+)?)(ms|s|m|h|d)$/);
-    if (durationMatch) {
-        const value = parseFloat(durationMatch[1]);
-        const unit = durationMatch[2];
-
-        switch (unit) {
-            case 'ms':
-                return value / 1000;
-            case 's':
-                return value;
-            case 'm':
-                return value * 60;
-            case 'h':
-                return value * 3600;
-            case 'd':
-                return value * 86400;
-        }
-    }
-
-    throw new Error("hs.timer.seconds(): unable to parse time string '" + timeString + "'");
-};
-
 // Predicate-based timers
 
 /// Repeat a function/lambda until a given predicate function/lambda returns true
@@ -188,51 +142,4 @@ hs.timer.waitWhile = function(predicateFn, actionFn, checkInterval) {
     });
 
     return timer.start();
-};
-
-// Delayed timer implementation - fires only after a period of inactivity
-// FIXME: This seems like a bad idea, and I'm pretty sure it's buggy, like `timer` should be inside `delayedObj`. Decide if we want this or not. Document if we do.
-/// SKIP_DOCS
-hs.timer.delayed = function(delay, fn) {
-    if (typeof fn !== 'function') {
-        throw new Error("hs.timer.delayed(): callback must be a function");
-    }
-
-    let timer = null;
-
-    const delayedObj = {
-        start: function(delayOverride) {
-            const actualDelay = delayOverride !== undefined ? delayOverride : delay;
-
-            if (timer) {
-                timer.stop();
-            }
-
-            timer = hs.timer.doAfter(actualDelay, fn);
-            return delayedObj;
-        },
-
-        stop: function() {
-            if (timer) {
-                timer.stop();
-                timer = null;
-            }
-            return delayedObj;
-        },
-
-        running: function() {
-            return timer ? timer.running() : false;
-        },
-
-        nextTrigger: function() {
-            return timer ? timer.nextTrigger() : -1;
-        },
-
-        setDelay: function(newDelay) {
-            delay = newDelay;
-            return delayedObj;
-        }
-    };
-
-    return delayedObj;
 };
